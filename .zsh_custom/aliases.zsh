bf1c9936ad89aacb1256f677767c24fe35bcf6a3
aab11 <ahmed@monbanquet.fr>
2018-06-29 18:48:43 +0100

    update zsh_custom and clean up

--- .zsh_custom/aliases.zsh
+++ .zsh_custom/aliases.zsh
@@ -1,3 +1,4 @@
 alias said="sudo"
 alias zshreload="source ~/.zshrc"
 alias tmux="TERM=screen-256color-bce tmux"
+alias git-main-branch="git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'"
